013a13017d1d1fcee43abc4bda67fc98612227a7
georgepelal <georgepelal@gmail.com>
2023-02-05 00:26:28 +0200

    Create ok.java

---
+++ ok.java
@@ -0,0 +1,12 @@
+public class ok {
+    public static void main(String[] args) {
+        String FName = "George ";
+        String Lname = "Pelal";
+        System.out.println(FName + Lname);
+        long ok=1270000000000000L;
+        System.out.println(ok);
+        boolean cond= true;
+        
+    }
+
+}
