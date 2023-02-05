a787a8ead45a7b95f570c0aeb05fa0a00e554a8e
georgepelal <georgepelal@gmail.com>
2023-02-05 02:45:18 +0200

    Update ok.java

--- ok.java
+++ ok.java
@@ -1,12 +1,38 @@
 public class ok {
     public static void main(String[] args) {
-        String FName = "George ";
-        String Lname = "Pelal";
-        System.out.println(FName + Lname);
-        long ok=1270000000000000L;
-        System.out.println(ok);
-        boolean cond= true;
-        
+
+        //String Length
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int num = txt.length();
+        System.out.println(num);
+
+        //String to lowercase
+        String newtxt =txt.toLowerCase();
+        System.out.println(newtxt);
+
+        //Locate word in a string
+        System.out.println(newtxt.indexOf("yz"));
+
+        //Random
+        System.out.println(Math.random() *150);
+
+        //--- if --- else --- else if ---
+        int x =40;
+        int y =30;
+        if (y>x){
+            System.out.println("y is bigger than x");
+        }else if ( x>y ) {
+            System.out.println("x is bigger than y");
+        }else {
+            System.out.println("x=y");
+        }
+
+        //quick if
+
+        String result = (y>x) ? "y is bigger than x" : "x is bigger than y";
+        System.out.println(result);
+
+
     }
 
 }
